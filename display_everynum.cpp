252b96344613ab9ae4c4fb54665c85d560678270
bingchun.zhao <759876426@qq.com>
2016-03-23 22:37:50 +0800

    this programer display ten numbers per line

--- display_everynum.cpp
+++ display_everynum.cpp
@@ -12,8 +12,12 @@ int main(int argc, char *argv[])
         lower = upper;
     }
     std::cout << "The numbers betwen " << lower << " and " << upper << " is " << std::endl;
-    for (int i = lower; i <= upper; i++) {
+    for (int i = lower, count = 1; i <= upper; count++, i++) {
         std::cout << i ;
+        if(count % 10 == 0)
+        {
+            std::cout << std::endl;
+        }
     }
     std::cout << std::endl;
     return 0;
